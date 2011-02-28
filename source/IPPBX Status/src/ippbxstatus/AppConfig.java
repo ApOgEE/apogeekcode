e5b9f6fe27d0eb63a88f1dc46cf88dcba29b04db
M. Fauzilkamil Zainuddin <jerungkun@gmail.com>
2011-02-28 16:51:10 +0800

    mulakan projek ut-ippbx

---
+++ source/IPPBX Status/src/ippbxstatus/AppConfig.java
@@ -0,0 +1,106 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ippbxstatus;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author apogee
+ */
+public class AppConfig {
+
+
+    private boolean bDebug = true;
+    private String ConfigFile = "ippbxstatus.conf";
+
+
+    public String IPPBXServerIP = "";
+    public String Username = "admin";
+    public String Password = "ultratone";
+    public String Extension = "";
+    public String ExtPassword = "";
+    public boolean EnableLogin = true;
+
+    public boolean ReadConfig() throws FileNotFoundException, IOException {
+        boolean bSuccess = false;
+
+        showDebug("DEBUG: Reading Config File!");
+        
+        // open config file if exists
+        Properties prop = new Properties();
+
+        try {
+            InputStream is;
+            is = new FileInputStream(ConfigFile);
+            // read config file
+            prop.load(is);
+
+            this.EnableLogin = Boolean.parseBoolean(prop.getProperty("enableLogin"));
+            this.ExtPassword = prop.getProperty("extpass");
+            this.Extension = prop.getProperty("ext");
+            this.Password = prop.getProperty("password");
+            this.Username = prop.getProperty("user");
+            this.IPPBXServerIP = prop.getProperty("server");
+
+
+        } catch (FileNotFoundException ex)  {
+            showDebug("Config Not Exists!");
+            // create config file
+            prop.setProperty("server", IPPBXServerIP.toString());
+            prop.setProperty("user", Username.toString());
+            prop.setProperty("password", Password.toString());
+            prop.setProperty("ext", Extension.toString());
+            prop.setProperty("extpass", ExtPassword.toString());
+            prop.setProperty("enableLogin", Boolean.toString(EnableLogin));
+
+            FileOutputStream fos = new FileOutputStream(ConfigFile);
+
+            prop.store(fos, "Ultratone IPPBX");
+        }
+
+        return true;
+    }
+
+    private void showDebug(String dbgMessage) {
+        if (bDebug == false) return;
+        javax.swing.JOptionPane.showMessageDialog(null, dbgMessage );
+    }
+
+    public void UpdateConfig() {
+        Properties prop = new Properties();
+
+        showDebug(IPPBXServerIP.toString());
+        prop.setProperty("server", IPPBXServerIP.toString());
+        prop.setProperty("user", Username.toString());
+        prop.setProperty("password", Password.toString());
+        prop.setProperty("ext", Extension.toString());
+        prop.setProperty("extpass", ExtPassword.toString());
+        prop.setProperty("enableLogin", Boolean.toString(EnableLogin));
+
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream(ConfigFile);
+            try {
+                prop.store(fos, "Ultratone IPPBX");
+            } catch (IOException ex) {
+                Logger.getLogger(AppConfig.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(AppConfig.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+    }
+         
+}
